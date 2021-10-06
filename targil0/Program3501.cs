4c71154885f26327c56fcf5112b8552815e578b6
nsx nsx <sz184790@gmail.com>
2021-10-06 10:14:46 +0300

    name of the first part

---
+++ targil0/Program3501.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome3501();
+            welcome0124();
+        }
+
+        static partial void welcome0124();
+        private static void welcome3501()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, Welcome to my first console application", name);
+        }
+    }
+}
