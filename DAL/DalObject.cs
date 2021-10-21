39df3b0215d943ae3a3830090366ddd0c3f88faf
nsx nsx <sz184790@gmail.com>
2021-10-21 22:55:10 +0300

    22:54  comman

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -8,11 +8,19 @@ using System.Threading.Tasks;
 namespace Dal
 {
     public class DalObject
-    {
+    {/// <summary>
+     /// This function activates the constructor of Initialize, so that all the Lists of the various entities are initialized.
+     /// </summary>
         public DalObject() => DataSource.Initialize();
+        /// <summary>
+        /// This function allows the user to add a base station to the list.
+        /// </summary>
+        /// <param name="station"></param>
+        /// <returns></true/false>
         public bool addStation(Station station)
-        {
+        {   
             int find = DataSource.stations.FindIndex(Station => Station.Id == station.Id);
+            //Safety mechanism to prevent the overrun of an existing entity with the same ID.
             if (find <= 0)
             {
                 DataSource.stations.Add(station);
@@ -20,10 +28,15 @@ namespace Dal
             }
             return false;
         }
-
+        /// <summary>
+        /// This function allows the user to add a drone to the list.
+        /// </summary>
+        /// <param name="drone"></param>
+        /// <returns></returns>
         public bool addDrone(Drone drone)
         {
             int find = DataSource.drones.FindIndex(Drone => Drone.Id == drone.Id);
+            //Safety mechanism to prevent the overrun of an existing entity with the same ID.
             if (find <= 0)
             {
                 DataSource.drones.Add(drone);
@@ -31,9 +44,15 @@ namespace Dal
             }
             return false;
         }
+        /// <summary>
+        /// This function allows the user to add a customer to the list.
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
         public bool addCustomer(Customer customer)
         {
             int find = DataSource.drones.FindIndex(Customer => Customer.Id == customer.Id);
+            //Safety mechanism to prevent the overrun of an existing entity with the same ID.
             if (find <= 0)
             {
                 DataSource.customers.Add(customer);
@@ -41,21 +60,37 @@ namespace Dal
             }
             return false;
         }
+        /// <summary>
+        ///This function allows the user to add a parcel to the list.
+        /// </summary>
+        /// <param name="parcel"></param>
+        /// <returns></returns>
         public int addParsel(Parcel parcel)
         {
             DataSource.parcels.Add(parcel);
             return (DataSource.Config.ParcelIdRun++);
         }
+        /// <summary>
+        /// This function assigns a package to the drone.
+        /// </summary>
+        /// <param name="parcel"></param>
+        /// <param name="drone"></param>
         public void AssignPackageToDrone(Parcel parcel, Drone drone)
         {
             parcel.DroneId = drone.Id;
             parcel.Scheduled = DateTime.Now;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parcel"></param>
+        /// <param name="drone"></param>
         public void PackageCollectionByDrone(Parcel parcel, Drone drone)
         {
             drone.Status = DroneStatuses.busy;
             parcel.PickedUp = DateTime.Now;
         }
+
         public void DeliveryPackageToCustomer(Parcel parcel, Drone drone)
         {
             parcel.Delivered = DateTime.Now;
@@ -115,28 +150,47 @@ namespace Dal
             }
             return temp;
         }
-        
+
         public List<Station> DisplaysIistOfBaseStations()
         {
             return getListTemplte<Station>(DataSource.stations);
         }
-        
+
         public List<Drone> DisplaysTheListOfDrons()
         {
             return getListTemplte<Drone>(DataSource.drones);
         }
-        
+
         public List<Customer> DisplaysIistOfCustomers()
         {
             return getListTemplte<Customer>(DataSource.customers);
         }
-        
+
         public List<Parcel> DisplaysIistOfparcels()
         {
             return getListTemplte<Parcel>(DataSource.parcels);
         }
 
-        
+        public List<Parcel> GetUnassignedPackages()
+        {
+            List<Parcel> UnassignedPackages = new List<Parcel>();
 
+            for (int i = 0; i < DataSource.parcels.Count; i++)
+            {
+                UnassignedPackages.Add(DataSource.parcels.Find(Parcel => Parcel.DroneId == 0));
+            }
+            return UnassignedPackages;
+        }
+
+        public List<Station> baseStationsWithAvailableChargingStations()
+        {
+            List<Station> freeChargingSlots = new List<Station>();
+
+            for (int i = 0; i < DataSource.stations.Count; i++)
+            {
+                freeChargingSlots.Add(DataSource.stations.Find(Station => Station.ChargeSlots > 0));
+            }
+            return freeChargingSlots;
+        }
     }
 }
