2c39c8f1bdc3a3d8d04123dc6628cd1fcbeff05a
יאיר בוסו <busso@g.jct.ac.il>
2021-11-09 14:43:59 +0200

    09/11/2021 14:41

---
+++ DAL/IDal.cs
@@ -0,0 +1,33 @@
+﻿using IDAL.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+
+	interface IDal
+	{
+		 bool addStation(Station station);
+		bool addDrone(Drone drone);
+		bool addCustomer(Customer customer);
+		int addParsel(Parcel parcel);
+		void addDroneCarge(DroneCarge droneCarge);
+		bool AssignPackageToDrone(int parcelId, int droneId);
+		bool PackageCollectionByDrone(int parcelId);
+		bool DeliveryPackageToCustomer(int parcelId);
+		bool ReleaseDroneCarge(int droneId);
+		bool addingCargeSlotsToStation(int stationId);
+		bool reductionCargeSlotsToStation(int stationId);
+		Station? getStation(int Id);
+		Drone? getDrone(int Id);
+		Customer? getCustomer(int Id);
+		Parcel? getParcel(int Id);
+		IEnumerable<Station> DisplaysIistOfStations(Predicate<Station> p = null);
+		IEnumerable<Drone> DisplaysTheListOfDrons(Predicate<Drone> p = null);
+		IEnumerable<Customer> DisplaysIistOfCustomers(Predicate<Customer> p = null);
+		IEnumerable<Parcel> DisplaysIistOfparcels(Predicate<Parcel> p = null);
+	}
+}
