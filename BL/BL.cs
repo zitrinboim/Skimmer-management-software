6f76495afdb390ae9a68ca2f1795e40d1824f66c
nsx nsx <sz184790@gmail.com>
2021-11-11 16:20:57 +0200

    16:20

--- BL/BL.cs
+++ BL/BL.cs
@@ -15,6 +15,8 @@ namespace BL
     {
         List<DroneToList> droneToList;
         IDAL.IDal dal;
+        DistanceAlgorithm d = new();
+
         internal static double available;
         internal static double easy;
         internal static double medium;
@@ -24,6 +26,7 @@ namespace BL
         {
             droneToList = new();
             dal = new Dal.DalObject();
+            Random random = new Random(DateTime.Now.Millisecond);
 
             double[] power = dal.PowerConsumptionRate();
             available = power[0];
@@ -40,35 +43,67 @@ namespace BL
                 droneToList[i].MaxWeight = drones[i].MaxWeight;
             }
             List<IDAL.DO.Parcel> PackagesInDelivery = dal.DisplaysIistOfparcels(i => (i.DroneId != 0 && i.Delivered == DateTime.MinValue)).ToList();
-            for (int i = 0; i < PackagesInDelivery.Count; i++)
+            for (int i = 0; i < PackagesInDelivery.Count; i++)//האם יש דרך ליעל את הפעולה הזו
             {
                 for (int j = 0; j < droneToList.Count; j++)
                 {
                     if (droneToList[j].Id==PackagesInDelivery[i].DroneId)
                     {
                         droneToList[j].DroneStatuses = DroneStatuses.busy;
+
+                        IDAL.DO.Customer sander = customers.Find(customer => customer.Id == PackagesInDelivery[i].SenderId);
+                        IDAL.DO.Customer target = customers.Find(customer => customer.Id == PackagesInDelivery[i].TargetId);
+                        Location sanderLocation = new() { latitude = sander.lattitude, longitude = sander.longitude };
+                        Location targetLocation = new() { latitude = target.lattitude, longitude = target.longitude };
+                        double battryOfDelivery;
+                        
+                       
                         if (PackagesInDelivery[i].PickedUp==DateTime.MinValue)
                         {
-                            droneToList[j].Location = TheNearestStation(PackagesInDelivery[i].SenderId);// יש לייצר את הפונקציה של חיפוש תחנה קרובה.
-                            //The nearest station
+                            droneToList[j].Location = TheNearestStation(sanderLocation);
+                            battryOfDelivery =(d.DistanceBetweenPlaces(droneToList[j].Location,sanderLocation)*available)
+                            +(d.DistanceBetweenPlaces(sanderLocation, targetLocation) * power[(int)PackagesInDelivery[i].weight])
+                            + (d.DistanceBetweenPlaces(targetLocation, TheNearestStation(targetLocation)) * available);
+                            droneToList[j].battery = (random.NextDouble() * (100.0 - battryOfDelivery)) + battryOfDelivery;
                         }
                         else
                         {
-                            IDAL.DO.Customer customer = customers.Find(customer => customer.Id == PackagesInDelivery[i].SenderId);
-                            droneToList[j].Location.longitude = customer.longitude;
-                            droneToList[j].Location.latitude = customer.lattitude;
-
+                            droneToList[j].Location= sanderLocation;
+                            battryOfDelivery = (d.DistanceBetweenPlaces(sanderLocation, targetLocation) * power[(int)PackagesInDelivery[i].weight])
+                            + (d.DistanceBetweenPlaces(targetLocation, TheNearestStation(targetLocation)) * available);
+                            droneToList[j].battery = (random.NextDouble() * (100.0 - battryOfDelivery)) + battryOfDelivery;
                         }
                     }
                 }
             }
+            foreach (DroneToList item in droneToList)
+            {
+                if (item.DroneStatuses != DroneStatuses.busy)
+                    item.DroneStatuses = (DroneStatuses)random.Next(1,3);   
+            }
+            
         }
         
-        private Location TheNearestStation(int sanderId)
+        internal Location TheNearestStation(Location customerLocation)
         {
-            List < IDAL.DO.Station> 
-            return Location;
+            List<IDAL.DO.Station> stations = dal.DisplaysIistOfStations().ToList();
+            IDAL.DO.Station closeStation = stations[0];
+            double distance = double.MaxValue;
+
+            for (int i = 0; i < stations.Count; i++)
+            {
+                Location stationLocation = new() { latitude = stations[i].lattitude, longitude = stations[i].longitude };
+                double distance1 = d.DistanceBetweenPlaces(stationLocation, customerLocation);
+                if (distance1<distance)
+                {
+                    distance = distance1;
+                    closeStation = stations[i];
+                }
+            }
+            Location location = new() { longitude = closeStation.longitude, latitude = closeStation.lattitude };
+            return location ;
         }
+        
 
         //public bool AddStation(Station station)
         //{
---
+++ BL/DistanceAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBL.BO;
+
+namespace BL
+{
+    internal class DistanceAlgorithm
+    {
+        const double PIx = 3.141592653589793;
+        const double RADIUS = 6378.16;
+
+        /// <summary>
+        /// Convert degrees to Radians
+        /// </summary>
+        /// <param name="x">Degrees</param>
+        /// <returns>The equivalent in radians</returns>
+        internal  double Radians(double x)
+        {
+            return x * PIx / 180;
+        }
+
+        /// <summary>
+        /// Calculate the distance between two places.
+        /// </summary>
+        /// <param name="lon1"></param>
+        /// <param name="lat1"></param>
+        /// <param name="lon2"></param>
+        /// <param name="lat2"></param>
+        /// <returns></returns>
+        internal  double DistanceBetweenPlaces( Location l1,Location l2)
+        {
+            double dlon = Radians(l2.longitude - l1.longitude);
+            double dlat = Radians(l2.longitude - l2.latitude);
+
+            double a = (Math.Sin(dlat / 2) * Math.Sin(dlat / 2)) + Math.Cos(Radians(l1.latitude)) * Math.Cos(Radians(l2.latitude)) * (Math.Sin(dlon / 2) * Math.Sin(dlon / 2));
+            double angle = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return angle * RADIUS;
+        }
+
+    }
+}
