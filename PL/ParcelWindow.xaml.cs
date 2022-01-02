1b9e92629cb131eb61aac7642cd37dda5f67037d
יאיר בוסו <busso@g.jct.ac.il>
2022-01-02 18:17:58 +0200

    18:17

--- PL/ParcelWindow.xaml.cs
+++ PL/ParcelWindow.xaml.cs
@@ -12,11 +12,11 @@ namespace PL
 {
     public struct parcelStatus_WeightCategories_Priorities
     {
-      public BO.parcelStatus parcelStatus { get; set; }
-    public BO.WeightCategories weight { get; set; }
-    public BO.Priorities priority { get; set; }
-        
-}
+        public BO.parcelStatus parcelStatus { get; set; }
+        public BO.WeightCategories weight { get; set; }
+        public BO.Priorities priority { get; set; }
+
+    }
     /// <summary>
     /// Interaction logic for PercelWindow.xaml
     /// </summary>
@@ -32,7 +32,7 @@ namespace PL
         public ParcelWindow(IBL bL, string _action = "")
         {
             parcelToListGroping = new();
-         
+
             blGui = bL;
             actions = new();
             action = _action;
@@ -52,41 +52,59 @@ namespace PL
                 //    Close.Content = "סגור";
                 //    actions = Actions.UPDATING;
                 //    break;
-                //case "Add":
-                //    AddWindow();
-                //    break;
+                case "Add":
+                    AddWindow();
+                    break;
 
                 default:
                     break;
             }
         }
+
         private void ListWindow()
         {
 
             ParcelListView.Items.Refresh();
             actions = Actions.LIST;
-            addButton.Content = "הוסף תחנה";
+            addButton.Content = "הוסף חבילה";
             Close.Content = "סגור";
             List.Visibility = Visibility.Visible;
             //Updating.Visibility = Visibility.Hidden;
             //Add.Visibility = Visibility.Hidden;
+
             ParcelListView.ItemsSource = parcelToListView;
             weightSelector.ItemsSource = Enum.GetValues(typeof(WeightCategories));
             prioritiSelector.ItemsSource = Enum.GetValues(typeof(Priorities));
             StatusSelector.ItemsSource = Enum.GetValues(typeof(parcelStatus));
 
-            weightSelector.SelectedIndex = 0;
-            prioritiSelector.SelectedIndex = 0;
-            StatusSelector.SelectedIndex = 0;
+            //weightSelector.SelectedIndex = 0;
+            //prioritiSelector.SelectedIndex = 0;
+            //StatusSelector.SelectedIndex = 0;
+
             //droneToListsView.CollectionChanged += DroneToListsView_CollectionChanged;
             SearchBy();
+        }
+
+        private void AddWindow()
+        {
+            actions = Actions.ADD;
+            addButton.Content = "הוסף";
+            Close.Content = "סגור";
+            List.Visibility = Visibility.Hidden;
+            //Updating.Visibility = Visibility.Hidden;
+            Add.Visibility = Visibility.Visible;
+            prioritiCombo.ItemsSource = Enum.GetValues(typeof(Priorities));
+            weightCombo.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+            parcel = new() { Id = 0, Requested = DateTime.Now };
+            DataContext = parcel;
         }
 
         public void InitList()//
         {
             IEnumerable<ParcelToList> temp = blGui.DisplaysIistOfparcels();
             parcelToListGroping = (from parcel in temp
-                                   group parcel by new parcelStatus_WeightCategories_Priorities { parcelStatus = parcel.parcelStatus, priority = parcel.priority, weight = parcel.weight })
+                                   group parcel by new parcelStatus_WeightCategories_Priorities
+                                   { parcelStatus = parcel.parcelStatus, priority = parcel.priority, weight = parcel.weight })
                                    .ToDictionary(X => X.Key, X => X.ToList());
 
         }
@@ -105,17 +123,102 @@ namespace PL
         {
             SearchBy();
         }
-        private void SearchBy() 
+        private void SearchBy()
         {
+            //WeightCategories weight = (WeightCategories)weightSelector.SelectedItem;
+            //parcelStatus parcelStatus = (parcelStatus)StatusSelector.SelectedItem;
+            //Priorities priorities = (Priorities)prioritiSelector.SelectedItem;
 
-            ParcelListView.ItemsSource = parcelToListGroping.Values.SelectMany(x => x);
+            //if (weight == WeightCategories.All && parcelStatus == parcelStatus.הכל && priorities == Priorities.הכל)
+            if (weightSelector.SelectedIndex == -1 && prioritiSelector.SelectedIndex == -1 && StatusSelector.SelectedIndex == -1)
+                ParcelListView.ItemsSource = parcelToListGroping.Values.SelectMany(x => x);
 
-            ParcelListView.ItemsSource = parcelToListGroping.Where(x => x.Key.parcelStatus == BO.parcelStatus.associated)  ;
+            // ParcelListView.ItemsSource = parcelToListGroping.Where(x => x.Key.parcelStatus == BO.parcelStatus.associated);
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
+            switch (actions)
+            {
+                case Actions.LIST:
+                    AddWindow();
+                    break;
+                case Actions.ADD:
 
+                    if (parcel.Sender.Id != default && parcel.Target.Id != default && parcel.weight != default && parcel.priority != default)//להעביר את הבדיקה לאיז אנעבלעד 
+                    {
+                        MessageBoxResult messageBoxResult = MessageBox.Show("האם ברצונך לאשר הוספה זו", "אישור", MessageBoxButton.OKCancel);//לשפר סטייל של ההודעה
+                        switch (messageBoxResult)
+                        {
+
+                            case MessageBoxResult.OK:
+                                _ = addParsel(parcel);
+                                parcelToList = GetParcelToList(parcel.Id);
+                                parcelToListView.Add(blGui.DisplaysIistOfparcels.First(i => i.Id == GetParcelToList(parcel.Id).Id));
+                                MessageBox.Show("הרחפן נוצר בהצלחה\n מיד תוצג רשימת הרחפנים", "אישור");
+                                ListWindow();
+                                break;
+                            case MessageBoxResult.Cancel:
+                                break;
+                            default:
+                                break;
+
+                        }
+                    }
+                    else
+                        MessageBox.Show("נא השלם את השדות החסרים", "אישור");
+                    break;
+                case Actions.UPDATING:
+                    if (addButton.Content == "הצג")
+                    {
+                        droneToList = droneToListsView.ToList().Find(i => i.Id == int.Parse(TxtBx_ID.Text.ToString()));
+                        if (droneToList.Id != 0)
+                        {
+
+                            BorderEnterNumber.Visibility = Visibility.Hidden;
+                            update.Visibility = Visibility.Visible;
+                            UpdatingWindow(droneToList.Id);
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("לא נמצא הרחפן", "אישור");
+                            Close();
+                        }
+                        break;
+                    }
+                    if (drone.Model != default)//איז אנעבעלד
+                    {
+                        MessageBoxResult messageBoxResult = MessageBox.Show("האם ברצונך לאשר עדכון זה", "אישור", MessageBoxButton.OKCancel);
+                        switch (messageBoxResult)
+                        {
+                            case MessageBoxResult.OK:
+                                if (relase.IsChecked == true)
+                                {
+                                    relase.IsChecked = false;
+                                    blGui.ReleaseDroneFromCharging(drone.Id, 7);
+                                    BorderStation.Visibility = Visibility.Hidden;
+                                }
+                                droneToList.Model = drone.Model;
+                                _ = blGui.updateModelOfDrone(droneToList.Model, droneToList.Id);
+                                // droneToListsView[index] = blGui.DisplaysIistOfDrons().First(i => i.Id == droneToList.Id);
+                                MessageBox.Show("העדכון בוצע בהצלחה\n מיד תוצג רשימת הרחפנים", "אישור");
+                                ListWindow();
+                                break;
+                            case MessageBoxResult.Cancel:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                        MessageBox.Show("נא השלם את השדות החסרים", "אישור");
+                    break;
+                case Actions.REMOVE:
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -125,7 +228,11 @@ namespace PL
 
         private void ParcelListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            ParcelToList parcelToList = (ParcelToList)ParcelListView.SelectedItem;
+            if (parcelToList != null)
+            {
+                //   UpdatingWindow(droneToList.Id);
+            }
         }
     }
 }
